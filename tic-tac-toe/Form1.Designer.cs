efd66277144c947270e1e54fdff214a61a403152
redlagoy <lagoyreden@gmail.com>
2021-05-23 13:18:09 +0800

    Add project files.

---
+++ tic-tac-toe/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace tic_tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTopLeft = new System.Windows.Forms.Button();
+            this.btnMidLeft = new System.Windows.Forms.Button();
+            this.btnBotLeft = new System.Windows.Forms.Button();
+            this.btnTopMid = new System.Windows.Forms.Button();
+            this.btnMidMid = new System.Windows.Forms.Button();
+            this.btnBotMid = new System.Windows.Forms.Button();
+            this.btnTopRight = new System.Windows.Forms.Button();
+            this.btnMidRight = new System.Windows.Forms.Button();
+            this.btnBotRight = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTopLeft
+            // 
+            this.btnTopLeft.AccessibleName = "";
+            this.btnTopLeft.Font = new System.Drawing.Font("Segoe UI Emoji", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnTopLeft.Location = new System.Drawing.Point(12, 12);
+            this.btnTopLeft.Name = "btnTopLeft";
+            this.btnTopLeft.Size = new System.Drawing.Size(158, 136);
+            this.btnTopLeft.TabIndex = 0;
+            this.btnTopLeft.UseVisualStyleBackColor = true;
+            this.btnTopLeft.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnMidLeft
+            // 
+            this.btnMidLeft.Location = new System.Drawing.Point(12, 154);
+            this.btnMidLeft.Name = "btnMidLeft";
+            this.btnMidLeft.Size = new System.Drawing.Size(158, 136);
+            this.btnMidLeft.TabIndex = 1;
+            this.btnMidLeft.UseVisualStyleBackColor = true;
+            // 
+            // btnBotLeft
+            // 
+            this.btnBotLeft.Location = new System.Drawing.Point(12, 296);
+            this.btnBotLeft.Name = "btnBotLeft";
+            this.btnBotLeft.Size = new System.Drawing.Size(158, 136);
+            this.btnBotLeft.TabIndex = 2;
+            this.btnBotLeft.UseVisualStyleBackColor = true;
+            this.btnBotLeft.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnTopMid
+            // 
+            this.btnTopMid.Location = new System.Drawing.Point(176, 12);
+            this.btnTopMid.Name = "btnTopMid";
+            this.btnTopMid.Size = new System.Drawing.Size(158, 136);
+            this.btnTopMid.TabIndex = 3;
+            this.btnTopMid.UseVisualStyleBackColor = true;
+            // 
+            // btnMidMid
+            // 
+            this.btnMidMid.Location = new System.Drawing.Point(176, 154);
+            this.btnMidMid.Name = "btnMidMid";
+            this.btnMidMid.Size = new System.Drawing.Size(158, 136);
+            this.btnMidMid.TabIndex = 4;
+            this.btnMidMid.UseVisualStyleBackColor = true;
+            // 
+            // btnBotMid
+            // 
+            this.btnBotMid.Location = new System.Drawing.Point(176, 296);
+            this.btnBotMid.Name = "btnBotMid";
+            this.btnBotMid.Size = new System.Drawing.Size(158, 136);
+            this.btnBotMid.TabIndex = 5;
+            this.btnBotMid.UseVisualStyleBackColor = true;
+            // 
+            // btnTopRight
+            // 
+            this.btnTopRight.Location = new System.Drawing.Point(340, 12);
+            this.btnTopRight.Name = "btnTopRight";
+            this.btnTopRight.Size = new System.Drawing.Size(158, 136);
+            this.btnTopRight.TabIndex = 6;
+            this.btnTopRight.UseVisualStyleBackColor = true;
+            // 
+            // btnMidRight
+            // 
+            this.btnMidRight.Location = new System.Drawing.Point(340, 154);
+            this.btnMidRight.Name = "btnMidRight";
+            this.btnMidRight.Size = new System.Drawing.Size(158, 136);
+            this.btnMidRight.TabIndex = 7;
+            this.btnMidRight.UseVisualStyleBackColor = true;
+            // 
+            // btnBotRight
+            // 
+            this.btnBotRight.Location = new System.Drawing.Point(340, 296);
+            this.btnBotRight.Name = "btnBotRight";
+            this.btnBotRight.Size = new System.Drawing.Size(158, 136);
+            this.btnBotRight.TabIndex = 8;
+            this.btnBotRight.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AccessibleName = "btnTopLeft";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 445);
+            this.Controls.Add(this.btnBotRight);
+            this.Controls.Add(this.btnMidRight);
+            this.Controls.Add(this.btnTopRight);
+            this.Controls.Add(this.btnBotMid);
+            this.Controls.Add(this.btnMidMid);
+            this.Controls.Add(this.btnTopMid);
+            this.Controls.Add(this.btnBotLeft);
+            this.Controls.Add(this.btnMidLeft);
+            this.Controls.Add(this.btnTopLeft);
+            this.Font = new System.Drawing.Font("Segoe UI Emoji", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTopLeft;
+        private System.Windows.Forms.Button btnMidLeft;
+        private System.Windows.Forms.Button btnBotLeft;
+        private System.Windows.Forms.Button btnTopMid;
+        private System.Windows.Forms.Button btnMidMid;
+        private System.Windows.Forms.Button btnBotMid;
+        private System.Windows.Forms.Button btnTopRight;
+        private System.Windows.Forms.Button btnMidRight;
+        private System.Windows.Forms.Button btnBotRight;
+    }
+}
+
