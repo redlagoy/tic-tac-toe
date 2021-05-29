9efbcba9192db3fbd4fedf5366268a986650bb7e
redlagoy <lagoyreden@gmail.com>
2021-05-29 11:23:32 +0800

    finalized design

--- tic-tac-toe/Form1.Designer.cs
+++ tic-tac-toe/Form1.Designer.cs
@@ -43,92 +43,101 @@ namespace tic_tac_toe
             // btnTopLeft
             // 
             this.btnTopLeft.AccessibleName = "";
+            this.btnTopLeft.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btnTopLeft.Font = new System.Drawing.Font("Segoe UI Emoji", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnTopLeft.Location = new System.Drawing.Point(12, 12);
             this.btnTopLeft.Name = "btnTopLeft";
             this.btnTopLeft.Size = new System.Drawing.Size(158, 136);
             this.btnTopLeft.TabIndex = 0;
-            this.btnTopLeft.UseVisualStyleBackColor = true;
+            this.btnTopLeft.UseVisualStyleBackColor = false;
             this.btnTopLeft.Click += new System.EventHandler(this.btnTopLeft_Click);
             // 
             // btnMidLeft
             // 
+            this.btnMidLeft.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btnMidLeft.Font = new System.Drawing.Font("Segoe UI Emoji", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnMidLeft.Location = new System.Drawing.Point(12, 154);
             this.btnMidLeft.Name = "btnMidLeft";
             this.btnMidLeft.Size = new System.Drawing.Size(158, 136);
             this.btnMidLeft.TabIndex = 1;
-            this.btnMidLeft.UseVisualStyleBackColor = true;
+            this.btnMidLeft.UseVisualStyleBackColor = false;
             this.btnMidLeft.Click += new System.EventHandler(this.btnMidLeft_Click);
             // 
             // btnBotLeft
             // 
+            this.btnBotLeft.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btnBotLeft.Font = new System.Drawing.Font("Segoe UI Emoji", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnBotLeft.Location = new System.Drawing.Point(12, 296);
             this.btnBotLeft.Name = "btnBotLeft";
             this.btnBotLeft.Size = new System.Drawing.Size(158, 136);
             this.btnBotLeft.TabIndex = 2;
-            this.btnBotLeft.UseVisualStyleBackColor = true;
+            this.btnBotLeft.UseVisualStyleBackColor = false;
             this.btnBotLeft.Click += new System.EventHandler(this.btnBotLeft_Click);
             // 
             // btnTopMid
             // 
+            this.btnTopMid.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btnTopMid.Font = new System.Drawing.Font("Segoe UI Emoji", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnTopMid.Location = new System.Drawing.Point(176, 12);
             this.btnTopMid.Name = "btnTopMid";
             this.btnTopMid.Size = new System.Drawing.Size(158, 136);
             this.btnTopMid.TabIndex = 3;
-            this.btnTopMid.UseVisualStyleBackColor = true;
+            this.btnTopMid.UseVisualStyleBackColor = false;
             this.btnTopMid.Click += new System.EventHandler(this.btnTopMid_Click);
             // 
             // btnMidMid
             // 
+            this.btnMidMid.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btnMidMid.Font = new System.Drawing.Font("Segoe UI Emoji", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnMidMid.Location = new System.Drawing.Point(176, 154);
             this.btnMidMid.Name = "btnMidMid";
             this.btnMidMid.Size = new System.Drawing.Size(158, 136);
             this.btnMidMid.TabIndex = 4;
-            this.btnMidMid.UseVisualStyleBackColor = true;
+            this.btnMidMid.UseVisualStyleBackColor = false;
             this.btnMidMid.Click += new System.EventHandler(this.btnMidMid_Click);
             // 
             // btnBotMid
             // 
+            this.btnBotMid.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btnBotMid.Font = new System.Drawing.Font("Segoe UI Emoji", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnBotMid.Location = new System.Drawing.Point(176, 296);
             this.btnBotMid.Name = "btnBotMid";
             this.btnBotMid.Size = new System.Drawing.Size(158, 136);
             this.btnBotMid.TabIndex = 5;
-            this.btnBotMid.UseVisualStyleBackColor = true;
+            this.btnBotMid.UseVisualStyleBackColor = false;
             this.btnBotMid.Click += new System.EventHandler(this.btnBotMid_Click);
             // 
             // btnTopRight
             // 
+            this.btnTopRight.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btnTopRight.Font = new System.Drawing.Font("Segoe UI Emoji", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnTopRight.Location = new System.Drawing.Point(340, 12);
             this.btnTopRight.Name = "btnTopRight";
             this.btnTopRight.Size = new System.Drawing.Size(158, 136);
             this.btnTopRight.TabIndex = 6;
-            this.btnTopRight.UseVisualStyleBackColor = true;
+            this.btnTopRight.UseVisualStyleBackColor = false;
             this.btnTopRight.Click += new System.EventHandler(this.btnTopRight_Click);
             // 
             // btnMidRight
             // 
+            this.btnMidRight.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btnMidRight.Font = new System.Drawing.Font("Segoe UI Emoji", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnMidRight.Location = new System.Drawing.Point(340, 154);
             this.btnMidRight.Name = "btnMidRight";
             this.btnMidRight.Size = new System.Drawing.Size(158, 136);
             this.btnMidRight.TabIndex = 7;
-            this.btnMidRight.UseVisualStyleBackColor = true;
+            this.btnMidRight.UseVisualStyleBackColor = false;
             this.btnMidRight.Click += new System.EventHandler(this.btnMidRight_Click);
             // 
             // btnBotRight
             // 
+            this.btnBotRight.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btnBotRight.Font = new System.Drawing.Font("Segoe UI Emoji", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnBotRight.Location = new System.Drawing.Point(340, 296);
             this.btnBotRight.Name = "btnBotRight";
             this.btnBotRight.Size = new System.Drawing.Size(158, 136);
             this.btnBotRight.TabIndex = 8;
-            this.btnBotRight.UseVisualStyleBackColor = true;
+            this.btnBotRight.UseVisualStyleBackColor = false;
             this.btnBotRight.Click += new System.EventHandler(this.btnBotRight_Click);
             // 
             // Form1
@@ -136,6 +145,7 @@ namespace tic_tac_toe
             this.AccessibleName = "btnTopLeft";
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
             this.ClientSize = new System.Drawing.Size(511, 445);
             this.Controls.Add(this.btnBotRight);
             this.Controls.Add(this.btnMidRight);
